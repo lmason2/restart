b32e1ea6aeda9cf38870524fd48b1ace69290ca0
Luke Mason <lukesamuelmason@gmail.com>
2021-12-21 07:48:52 -0700

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Luke Mason on 12/21/21.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
